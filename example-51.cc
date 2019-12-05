2501ca032d44768a41f8aa7bacd8aac069f2f9bd
Andy Soffer <asoffer@google.com>
2019-12-05 16:01:50 -0500

    Initial examples

---
+++ example-51.cc
@@ -0,0 +1,22 @@
+#include <cstdio>
+
+namespace ns {
+class MyClass {
+ public:
+  int violation_public_class_member_ = 0;
+
+  MyClass(int violation_non_explicit) : data_(violation_non_explicit) {}
+
+  int data() const { return data_; }
+
+ private:
+  int data_ = 0;
+};
+// Violation closing namespace
+}
+
+int main() {
+  ns::MyClass m(51);
+
+  return m.violation_public_class_member_ + m.data();
+}
